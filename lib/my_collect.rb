8c6ef2d89689511abea9fb11644fca3f2410096d
Fenty Hall <fentymhall@gmail.com>
2020-07-31 03:55:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(collection)
+  i = 0 
+  array = [] 
+  while i < collection.length 
+    array << yield(collection[i])
+    i += 1 
+  end 
+  array
+end 
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
