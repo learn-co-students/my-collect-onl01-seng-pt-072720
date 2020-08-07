aba7812c38e7c9a3d25f7cab48603bc07f22f862
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 19:29:52 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  conclusion = []
+  while i < array.length
+    conclusion.push(yield array[i])
+      i += 1
+  end
+  conclusion
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
